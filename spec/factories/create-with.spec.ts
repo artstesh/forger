699a792ad469d941824376a39b4de8ae0403d5e1
artstesh <artstesh@mail.ru>
2022-11-02 02:36:08 +0300

    Without 2

--- spec/factories/create-with.spec.ts
+++ spec/factories/create-with.spec.ts
@@ -1,12 +1,11 @@
 import { Forger } from '../../src/forger';
-import { ForgerType } from '../../src/models/forger.type';
 
 
 describe('createWith', () => {
 
   it('set specific value', () => {
     interface Test {prop: string}
-    const expected = Forger.create<string>({}, 0, {type: ForgerType.String})!;
+    const expected = Forger.create<string>()!;
     const obj = Forger.createWith<Test>().with(t => t.prop = expected).result();
     //
     expect(obj!.prop).toEqual(expected);
@@ -14,9 +13,10 @@ describe('createWith', () => {
 
   it('set a few values', () => {
     interface Test {prop1: string,prop2: string}
-    const expected1 = Forger.create<string>({}, 0, {type: ForgerType.String})!;
-    const expected2 = Forger.create<string>({}, 0, {type: ForgerType.String})!;
-    const obj = Forger.createWith<Test>().with(t => t.prop1 = expected1)
+    const expected1 = Forger.create<string>()!;
+    const expected2 = Forger.create<string>()!;
+    const obj = Forger.createWith<Test>()
+      .with(t => t.prop1 = expected1)
       .with(t => t.prop2 = expected2).result();
     //
     expect(obj!.prop1).toEqual(expected1);
@@ -27,7 +27,7 @@ describe('createWith', () => {
     interface Test {prop: string, other: number}
     const obj = Forger.createWith<Test>().with(t => t.prop = '').result();
     //
-    expect(obj!.other).toBeDefined();
+    expect(obj!.other).toBeTruthy();
   })
 
   it('allow generate with create inside', () => {
@@ -40,7 +40,7 @@ describe('createWith', () => {
 
   it('set value for root instance of type only', () => {
     interface Test {prop: string, inner: Test}
-    const expected = Forger.create<string>({}, 1, {type: ForgerType.String})!;
+    const expected = Forger.create<string>()!;
     const obj = Forger.createWith<Test>().with(t => t.prop = expected).result()!;
     //
     expect(obj.inner.prop).not.toEqual(obj.prop);
@@ -50,9 +50,8 @@ describe('createWith', () => {
   it('not set value with the same name for other types', () => {
     interface Inner {prop: string}
     interface Test {prop: string, inner: Inner}
-    const expected = Forger.create<string>({}, 0, {type: ForgerType.String})!;
-    const obj = Forger.createWith<Test>()
-      .with(t => t.prop = expected).result();
+    const expected = Forger.create<string>({}, 0)!;
+    const obj = Forger.createWith<Test>().with(t => t.prop = expected).result();
     //
     expect(obj!.inner.prop).not.toEqual(expected);
     expect(obj!.inner.prop).toBeDefined();
--- spec/partial.spec.ts
+++ spec/partial.spec.ts
@@ -3,10 +3,11 @@ import { Forger } from '../src';
 describe('partial#', () => {
 
     it('inner generics success', () => {
-        interface Test {prop: string, inner: Test}
-        const obj = Forger.createWith<Test>().with(t => t.prop = '').result()!;
+        interface Test {prop: string, other: string}
+        const obj = Forger.createWith<Test>({stringLength: 2})
+          .with(t => t.prop = Forger.create<string>({stringSpecial: false})!)
+          .result();
         //
-        expect(obj.inner.prop).not.toEqual(obj.prop);
-        expect(obj.inner.prop).toBeDefined();
+        expect(obj!.other).toBeTruthy();
     });
 })
